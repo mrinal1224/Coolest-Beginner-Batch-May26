81fca1c161efa999b75bb755bf0aca6c5b2132d5
mrinal1224 <iammrinalkc@gmail.com>
2025-10-17 22:28:31 +0530

    get method with route parameters

--- LLD-4/class-2/server/index.js
+++ LLD-4/class-2/server/index.js
@@ -1,10 +1,44 @@
 const express = require('express')
 
 const app = express()
+
+const courses = [
+    {id:1 , name:'Java' , instructor:"Atif" , ratings:4.8 , isActive:true},
+    {id:2 , name:'JavaScript' , instructor:"Samprit" , ratings:4.9 , isActive:true},
+    {id:3 , name:'DBMS' , instructor:"Ekta" , ratings:4.85 , isActive:false},
+]
+ 
+
+
 // get request
-app.get('/' , (req , res)=>{
+app.get('/topics' , (req , res)=>{
     res.send("Hello from the Express Server")
 })
+
+// getting all courses
+app.get('/topics/courses' , (req , res)=>{
+   res.send(courses)
+})
+
+// get a specific course
+// Route Parmeters
+
+app.get('/topics/courses/:id' , (req ,res)=>{
+    console.log(req.params) //id{2}
+    let course = courses.find((course)=>course.id===parseInt(req.params.id))
+
+    if(!course){
+        return res.send('Course with the id not found')
+    }
+    res.send(course)
+})
+
+
+
+// How to create something  - post 
+
+
+
 
 //more http methods and more properties
 
